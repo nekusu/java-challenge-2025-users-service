9299b7377d87af55d17f247091927e9c1d5260c0
nekusu <nekusu.dev@gmail.com>
2025-04-22 10:23:12 -0300

    Initialize users service with Spring Boot

---
+++ src/test/java/com/sharks/users_service/UsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharks.users_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
